3c7c9af2f7b93a52277b5e58f3a2cbb8693f7e19
FiLl0s0FeAr <vivasu5@ukr.net>
2022-02-17 20:35:55 +0200

    Google home work

---
+++ hm1/src/App.js
@@ -0,0 +1,11 @@
+import Google from "./components/Google";
+import './index.css';
+function App() {
+  return (
+    <div className="App">
+        <Google />
+    </div>
+  );
+}
+
+export default App;
---
+++ hm1/src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <>
+            <input className='input'>
+            </input>
+        </>
+    );
+};
+
+export default Input;
